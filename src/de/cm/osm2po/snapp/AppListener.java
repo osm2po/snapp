3e57ec627f462675e4aaf745595f9e68092b84f0
Carsten <cmindividual@web.de>
2015-10-12 20:31:03 +0200

    Test

--- src/de/cm/osm2po/snapp/AppListener.java
+++ src/de/cm/osm2po/snapp/AppListener.java
@@ -1,6 +1,6 @@
 package de.cm.osm2po.snapp;
 
-public interface AppListener {
+public interface  AppListener {
 	void onGpsSignal(double lat, double lon, float bearing);
 	void onPathPositionChanged(double lat, double lon, float bearing);
 	void onModeChanged();
